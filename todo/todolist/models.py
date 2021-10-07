736460e953e6722af9a0bb8e496ef861f5bf7997
Aidaiym <omurbekovnaaidaiym@gmail.com>
2021-10-07 09:17:31 +0600

    To do list app models

---
+++ todo/todolist/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+class Profile(models.Model):
+    CHOICES = (
+        ('Edit', 'Edit'),
+        ('Delete', 'Delete'),
+        ('Add', 'Add'),
+    )
+    name = models.CharField(max_length=300)
+    options = models.CharField(max_length=300, choices = CHOICES)
+    created_date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.name 
