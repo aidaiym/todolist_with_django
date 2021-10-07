ee0f941ab0774e1a6bcdc555247880914f2bf12b
Aidaiym <omurbekovnaaidaiym@gmail.com>
2021-10-07 16:01:03 +0600

    Editing, Deleting, Add  - - - commands

--- todo/todolist/models.py
+++ todo/todolist/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-class Profile(models.Model):
-    CHOICES = (
-        ('Edit', 'Edit'),
-        ('Delete', 'Delete'),
-        ('Add', 'Add'),
-    )
-    username = models.CharField(max_length=300)
-    options = models.CharField(max_length=300, choices = CHOICES)
-    created_date = models.DateTimeField(default=timezone.now)
+# class Profile(models.Model):
+#     CHOICES = (
+#         ('Edit', 'Edit'),
+#         ('Delete', 'Delete'),
+#         ('Add', 'Add'),
+#     )
+#     username = models.CharField(max_length=300)
+#     options = models.CharField(max_length=300, choices = CHOICES)
+#     created_date = models.DateTimeField(default=timezone.now)
+#     def __str__(self):
+#         return self.username 
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title=models.CharField(max_length=350)
+    completed=models.BooleanField(default=False)
+    created=models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.username 
+        return self.title
