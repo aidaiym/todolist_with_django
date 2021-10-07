b99f456b3385e2c871eecd9dc391ae41edc7a2fc
Aidaiym <omurbekovnaaidaiym@gmail.com>
2021-10-07 12:59:07 +0600

    Added html and css

--- todo/todolist/views.py
+++ todo/todolist/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Profile
+def todoappView(request):
+    profile = Profile.objects.fiter
+    return render(request, 'blog/todolist.html', {'Profile':Profile})
 # Create your views here.
